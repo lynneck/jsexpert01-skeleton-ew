5d9f5ac5099b6e109a51cc7429271b69c31828eb
lynneck <lynneckmartins@.com>
2023-11-17 16:08:27 -0300

    commit end

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -1,13 +1,50 @@
+/*const MANIFEST_URL = 'manifest.json'
+const localHost = ['127.0.0.1', 'localhost']
+
 async function main() {
-    const player = videojs('vid');
-    const ModalDialog = videojs.getComponent('ModalDialog');
-    const modal = new ModalDialog(player, {
-        temporary: false, 
-        closeable: true
-    });
+    const isLocal = !!~localHost.indexOf(window.location.hostname)  
+    console.log('isLocal?', isLocal)
+    const manifestJSON = await (await fetch(MANIFEST_URL)).json()
+    const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
+    const videoComponent = new VideoComponent()
+    const network = new Network({ host })
+    const videoPlayer = new VideoMediaPlayer({
+                             
+        manifestJSON,
+        network,
+        videoComponent
+    })
 
-    player.addChild(modal);
+    videoPlayer.initializeCodec() 
+                 
+    videoComponent.initializePlayer()
 
+}
+
+window.onload = main
+
+*/
+
+const MANIFEST_URL = 'manifest.json'
+const localHost = ['127.0.0.1', 'localhost']
+
+async function main() {
+    const isLocal = !!~localHost.indexOf(window.location.hostname)  
+    console.log('isLocal?', isLocal)
+    const manifestJSON = await (await fetch(MANIFEST_URL)).json()
+    const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
+    const videoComponent = new VideoComponent()
+    const network = new Network({ host })
+    const videoPlayer = new VideoMediaPlayer({
+        manifestJSON,
+        network,
+        videoComponent
+    })
+
+    videoPlayer.initializeCodec()
+    videoComponent.initializePlayer()
+
+    window.nextChunk = (data) => videoPlayer.nextChunk(data)
 }
 
 window.onload = main
